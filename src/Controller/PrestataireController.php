656bb6c3159020ecc18d6048dc5f7a0c1bc828dc
cindy <remycindy@gmail.com>
2022-12-17 17:00:12 +0100

    creation controller prestataire

---
+++ src/Controller/PrestataireController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class PrestataireController extends AbstractController
+{
+    /**
+     * @Route("/prestataire", name="app_prestataire")
+     */
+    public function index(): Response
+    {
+        return $this->render('prestataire/index.html.twig', [
+            'controller_name' => 'PrestataireController',
+        ]);
+    }
+}
